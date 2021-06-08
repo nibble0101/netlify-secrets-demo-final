1fdecd181825cdccff27a5e152a3d0723d69a186
nibble0101 <mj.mawa@yahoo.com>
2021-06-08 21:54:42 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todoId, setTodoId] = useState(1);
+  const [todo, setTodo] = useState("");
+
+  function getNewTodo() {
+    if(todo === 20){
+      setTodo(1);
+      return;
+    };
+    setTodoId((todoId) => todoId + 1);
+  }
+
+  useEffect(() => {
+    async function fetchTodo() {
+      const url = `${process.env.REACT_APP_BASE_URL}/${todoId}`;
+      try {
+        const todo = await fetch(url).then((res) => res.json());
+        setTodo(todo.title);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchTodo();
+  }, [todoId]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <button onClick={getNewTodo}> Get another todo </button>
+      </p>
+      <p>{todo}</p>
     </div>
   );
 }
